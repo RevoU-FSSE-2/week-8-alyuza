b1c4939fa8f6a35676bf124e612cd4a90972f39f
alyuza <alyuza.sat@gmail.com>
2023-08-11 23:30:32 +0700

    add data.js

---
+++ dist/data.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.transactions = void 0;
+;
+// Input data
+exports.transactions = [
+    { id: 1, name: "Alyuza Satrio Prayogo", inputType: "Cash In", amount: 9500000, description: "Salary" },
+    { id: 2, name: "Alyuza Satrio Prayogo", inputType: "Cash Out", amount: 2000000, description: "Pay for housing" },
+    { id: 3, name: "Alyuza Satrio Prayogo", inputType: "Cash Out", amount: 1300000, description: "Buy food for 1 month" },
+    { id: 4, name: "Alyuza Satrio Prayogo", inputType: "Cash Out", amount: 500000, description: "Pay internet" },
+];
