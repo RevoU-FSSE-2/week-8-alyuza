2953e7de0de11100f51d252cc952f82aa2879668
alyuza <alyuza.sat@gmail.com>
2023-08-12 11:02:33 +0700

    compile

--- dist/data.js
+++ dist/data.js
@@ -2,10 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.transactions = void 0;
 ;
-// Input data
-exports.transactions = [
-    { id: 1, name: "Alyuza Satrio Prayogo", inputType: "Cash In", amount: 9500000, description: "Salary" },
-    { id: 2, name: "Alyuza Satrio Prayogo", inputType: "Cash Out", amount: 2000000, description: "Pay for housing" },
-    { id: 3, name: "Alyuza Satrio Prayogo", inputType: "Cash Out", amount: 1300000, description: "Buy food for 1 month" },
-    { id: 4, name: "Alyuza Satrio Prayogo", inputType: "Cash Out", amount: 500000, description: "Pay internet" },
+// Input data (untuk di export ke index.ts)
+let transactions = [
+    { transId: 1, name: "Alyuza Satrio Prayogo", inputType: "Cash In", amount: 9500000, description: "Salary" },
+    { transId: 2, name: "Alyuza Satrio Prayogo", inputType: "Cash Out", amount: 2000000, description: "Pay for housing" },
+    { transId: 3, name: "Alyuza Satrio Prayogo", inputType: "Cash Out", amount: 1300000, description: "Buy food for 1 month" },
+    { transId: 4, name: "Alyuza Satrio Prayogo", inputType: "Cash Out", amount: 500000, description: "Pay internet" },
 ];
+exports.transactions = transactions;
