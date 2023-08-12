f986d8fc3ed3630cf9a86cf964d6d321741b0be6
alyuza <alyuza.sat@gmail.com>
2023-08-12 20:57:36 +0700

    modify data.ts

--- data.ts
+++ data.ts
@@ -1,17 +1,16 @@
+// Interface / bisa menggunakan Type
 interface Transaction {
     transId: number,
     name: string,
-    inputType: string,
-    amount: number,
-    description: string,
+    inputType : string,
+    amount:number,
+    description:string,
 };
 
 // Input data (untuk di export ke index.ts)
-let transactions: Transaction[] = [
-    { transId: 1, name: "Alyuza Satrio Prayogo", inputType: "Cash In", amount: 9500000, description: "Salary" },
-    { transId: 2, name: "Alyuza Satrio Prayogo", inputType: "Cash Out", amount: 2000000, description: "Pay for housing" },
-    { transId: 3, name: "Alyuza Satrio Prayogo", inputType: "Cash Out", amount: 1300000, description: "Buy food for 1 month" },
-    { transId: 4, name: "Alyuza Satrio Prayogo", inputType: "Cash Out", amount: 500000, description: "Pay internet" },
+export let transactions:Transaction[] = [
+    {transId:1, name:"Alyuza Satrio Prayogo", inputType:"Cash In", amount:9500000, description:"Salary"},
+    {transId:2, name:"Alyuza Satrio Prayogo", inputType:"Cash Out", amount:2000000, description:"Pay for housing"},
+    {transId:3, name:"Alyuza Satrio Prayogo", inputType:"Cash Out", amount:1300000, description:"Buy food for 1 month"},
+    {transId:4, name:"Alyuza Satrio Prayogo", inputType:"Cash Out", amount:500000, description:"Pay internet"},
 ]
-
-export { Transaction, transactions }
